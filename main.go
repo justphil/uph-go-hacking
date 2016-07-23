660d67ecdd52fa03a86264e6cbacdf9ad745327f
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2016-07-23 11:03:50 +0200

    add http endpoint

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+)
 
 func main() {
-	fmt.Println("TEST")
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	//
+
+	e.Run(standard.New(":1323"))
 }
