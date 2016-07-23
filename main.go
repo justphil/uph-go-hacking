8d8fe522658d380fa5fd011cffce20b8847c4e9c
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2016-07-23 11:38:41 +0200

    update main.go

--- main.go
+++ main.go
@@ -24,11 +24,3 @@ func main() {
 
 	e.Run(standard.New(":1323"))
 }
-
-type MachineAccount struct {
-	Name       string `json:"name"`
-	Email      string `json:"email"`
-	IP         string `json:"ip"`
-	OsUser     string `json:"osUser"`
-	OsPassword string `json:"osPassword"`
-}
