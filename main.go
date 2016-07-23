6948c07c8d7ab0d14a636fd5ddad06da187920e4
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2016-07-23 11:30:36 +0200

    add machine account struct

--- main.go
+++ main.go
@@ -11,10 +11,24 @@ func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		machineAccount := MachineAccount{
+			Name:       "test",
+			Email:      "foo@bar.com",
+			IP:         "127.0.0.1",
+			OsUser:     "pi",
+			OsPassword: "raspberry",
+		}
+
+		return c.JSON(http.StatusOK, machineAccount)
 	})
 
-	//
-
 	e.Run(standard.New(":1323"))
+}
+
+type MachineAccount struct {
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	IP         string `json:"ip"`
+	OsUser     string `json:"osUser"`
+	OsPassword string `json:"osPassword"`
 }
