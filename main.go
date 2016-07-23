8473722cfdae10e56f1bfa0e6309764f6d415de5
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2016-07-23 11:48:27 +0200

    add skeleton functions for http handlers

--- main.go
+++ main.go
@@ -12,17 +12,39 @@ import (
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		machineAccount := models.MachineAccount{
-			Name:       "test",
-			Email:      "foo@bar.com",
-			IP:         "127.0.0.1",
-			OsUser:     "pi",
-			OsPassword: "raspberry",
-		}
-
-		return c.JSON(http.StatusOK, machineAccount)
-	})
+	e.GET("/machine-accounts", getMachineAccounts)
+	e.GET("/machine-accounts/:name", getMachineAccountByName)
+	e.POST("/machine-accounts", createMachineAccount)
+	e.PUT("/machine-accounts/:name", updateMachineAccountByName)
+	e.DELETE("/machine-accounts/:name", deleteMachineAccountByName)
 
 	e.Run(standard.New(":1323"))
+}
+
+func getMachineAccounts(c echo.Context) error {
+	machineAccount := models.MachineAccount{
+		Name:       "test",
+		Email:      "foo@bar.com",
+		IP:         "127.0.0.1",
+		OsUser:     "pi",
+		OsPassword: "raspberry",
+	}
+
+	return c.JSON(http.StatusOK, machineAccount)
+}
+
+func getMachineAccountByName(c echo.Context) error {
+	return nil
+}
+
+func createMachineAccount(c echo.Context) error {
+	return nil
+}
+
+func updateMachineAccountByName(c echo.Context) error {
+	return nil
+}
+
+func deleteMachineAccountByName(c echo.Context) error {
+	return nil
 }
